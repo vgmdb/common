fa1a086a15b3ecd94978928454d3584dde32aa3c
Chris Heng <bigblah@gmail.com>
2013-08-20 15:08:59 +0800

    Remove unused use statement

--- src/VGMdb/Component/Doctrine/Command/DoctrineBuildCommand.php
+++ src/VGMdb/Component/Doctrine/Command/DoctrineBuildCommand.php
@@ -7,7 +7,6 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use MwbExporter\Bootstrap;
 use MwbExporter\Formatter\Doctrine2\Annotation\Formatter;
 
