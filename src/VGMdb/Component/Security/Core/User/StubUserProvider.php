ed85392446937ae6a321a8f7b6872cdfdd94eac0
Chris Heng <bigblah@gmail.com>
2013-11-05 14:08:03 +0800

    Fix missing namespace declaration

--- src/VGMdb/Component/Security/Core/User/StubUserProvider.php
+++ src/VGMdb/Component/Security/Core/User/StubUserProvider.php
@@ -6,6 +6,7 @@ use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\User;
 use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 
 /**
  * Placeholder user entity provider.
